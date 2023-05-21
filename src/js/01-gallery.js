17c61f37d060bfc39c5590fe100ca6eeaec4da7f
MichaelMikhieienko <bsmmmv@gmail.com>
2023-05-21 12:15:00 +0300

    Correct

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// Change code below this line
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+          <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+              />
+            </a>
+          </li>
+        `;
+    })
+    .join('');
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+const container = document.querySelector('.gallery');
+container.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a');
+
