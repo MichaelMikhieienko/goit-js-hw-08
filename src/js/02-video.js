f368987e488e9146eb014a3fd755159c6a993993
MichaelMikhieienko <bsmmmv@gmail.com>
2023-05-21 18:38:17 +0300

    Correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,28 @@
-import Player from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 import { throttle } from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
-const player = new Player(iframe);
+const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', throttle(function(event) {
-  const currentTime = event.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
 }, 1000));
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
-
 if (savedTime) {
-  const currentTime = parseFloat(savedTime);
-  player.setCurrentTime(currentTime);
+    const currentTime = parseFloat(savedTime);
+    player.setCurrentTime(currentTime).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                console.log('Invalid time value');
+                break;
+            default:
+                console.log('Error occurred:', error);
+                break;
+        }
+    });
 }
+
+
 
